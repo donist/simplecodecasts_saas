e3cd6ac961e9456bd9d3c4fee15f66489e521a57
Don McMillan <donist40@gmail.com>
2015-09-15 20:14:48 +0000

    updated routes file with new devise controller

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150914153743) do
+ActiveRecord::Schema.define(version: 20150915195831) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
